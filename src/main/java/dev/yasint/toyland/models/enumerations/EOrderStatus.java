c3206facf240d6b265d5594d50504a630ddb0a88
Ricard Helvid Adrian <ricard.helvid@gmail.com>
2022-11-24 13:02:10 +0000

    Order module: add and get orders (#13)

---
+++ src/main/java/dev/yasint/toyland/models/enumerations/EOrderStatus.java
@@ -0,0 +1,10 @@
+package dev.yasint.toyland.models.enumerations;
+
+public enum EOrderStatus {
+    CREATED,
+    PACKAGING,
+    PACKAGING_COMPLETE,
+    DELIVERING,
+    DELIVERY_COMPLETED,
+    CANCELLED
+}
