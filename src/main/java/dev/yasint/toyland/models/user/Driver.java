5a39e500ded008152fcb32cfad10d2403e1cf11b
Mejabi Oluwadurotimi <durotimitech@gmail.com>
2022-11-27 17:36:50 +0000

    feat: add location

--- src/main/java/dev/yasint/toyland/models/user/Driver.java
+++ src/main/java/dev/yasint/toyland/models/user/Driver.java
@@ -1,13 +1,14 @@
 package dev.yasint.toyland.models.user;
 
+import dev.yasint.toyland.models.Location;
+import dev.yasint.toyland.models.Order;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
@@ -18,5 +19,33 @@ public class Driver {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String licensePlate;
+
+    /**
+     * A driver's persistent contact. This can only be
+     * changed by an admin.
+     */
+    @OneToOne
+    @JoinColumn(
+            name = "fk_driver_contact",
+            referencedColumnName = "id"
+    )
+    private Contact contact;
+
+    @OneToOne
+    @JoinColumn(
+            name = "fk_driver_location",
+            referencedColumnName = "id"
+    )
+    private Location location;
+
+    @OneToMany
+    @JoinTable(
+            name = "order_mapping_deliveries",
+            joinColumns = @JoinColumn(name = "driver_id"),
+            inverseJoinColumns = @JoinColumn(name = "delivery_order_id")
+    )
+    private Set<Order> deliveries = new HashSet<>();
 
 }
