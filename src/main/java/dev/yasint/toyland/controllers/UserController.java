fbd7406db8f3879159e53520f2579fd46e1f714a
Ricard <ricard.helvid@gmail.com>
2022-12-02 21:15:16 +0000

    move register-driver API from auth controller to user controller

--- src/main/java/dev/yasint/toyland/controllers/UserController.java
+++ src/main/java/dev/yasint/toyland/controllers/UserController.java
@@ -2,6 +2,7 @@ package dev.yasint.toyland.controllers;
 
 import dev.yasint.toyland.dtos.request.ContactUpdateDTO;
 import dev.yasint.toyland.dtos.request.PaymentUpdateDTO;
+import dev.yasint.toyland.dtos.request.UserSignupReqDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface UserController {
@@ -11,5 +12,7 @@ public interface UserController {
     ResponseEntity<?> updateContact(ContactUpdateDTO body);
 
     ResponseEntity<?> updatePayment(PaymentUpdateDTO body);
+
+    ResponseEntity<?> registerDriver(UserSignupReqDTO body);
 
 }
