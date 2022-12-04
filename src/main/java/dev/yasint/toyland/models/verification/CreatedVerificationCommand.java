0778b92974ec960a29a37b2c6ef3ac967cb722a1
durotimitech <durotimitech@gmail.com>
2022-12-04 17:23:48 +0000

    added_command_pattern

--- src/main/java/dev/yasint/toyland/models/verification/CreatedVerificationCommand.java
+++ src/main/java/dev/yasint/toyland/models/verification/CreatedVerificationCommand.java
@@ -2,6 +2,10 @@ package dev.yasint.toyland.models.verification;
 
 import dev.yasint.toyland.models.enumerations.EVerificationStatus;
 
+/**
+ * Triggered when a verification request is initiated
+ */
+
 public class CreatedVerificationCommand extends VerificationCommand {
 
     public CreatedVerificationCommand(Verification verification) {
